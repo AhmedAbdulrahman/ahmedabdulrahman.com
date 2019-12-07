e8e313b8f2608fca23da2caf6d619f8303601307
Ahmed Abdulrahman <a.kasapbashi@live.com>
2019-12-08 00:16:25 +0100

    :zap: [Components|Templates] adds category components & templates

---
+++ @narative/gatsby-theme-novela/src/templates/category.template.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+import Section from '@components/Section';
+import SEO from '@components/SEO';
+import Layout from '@components/Layout';
+import Paginator from '@components/Navigation/Navigation.Paginator';
+import NavCategory from '@components/Navigation/Navigation.Categories';
+
+import ArticlesList from '../sections/articles/Articles.List';
+// import CategoryHero from '../sections/article/Category.Hero';
+import ArticlesHero from "../sections/articles/Articles.Hero";
+
+function CategoryPage({ location, pageContext }) {
+  const { group: articles, category } = pageContext;
+  const authors = pageContext.additionalContext.authors;
+
+  return (
+    <Layout>
+      <SEO pathname={location.pathname} title={category} />
+      <ArticlesHero authors={authors} />
+      <Section narrow>
+        <NavCategory category={category} />
+        <ArticlesList articles={articles} />
+        <ArticlesPaginator show={pageContext.pageCount > 1}>
+          <Paginator {...pageContext} />
+        </ArticlesPaginator>
+      </Section>
+      <ArticlesGradient />
+    </Layout>
+  );
+}
+
+export default CategoryPage;
+
+const ArticlesGradient = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 590px;
+  z-index: 0;
+  pointer-events: none;
+  background: ${p => p.theme.colors.gradient};
+  transition: ${p => p.theme.colorModeTransition};
+`;
+
+const ArticlesPaginator = styled.div<{ show: boolean }>`
+  ${p => p.show && `margin-top: 64px;`}
+`;
