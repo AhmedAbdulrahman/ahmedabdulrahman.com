7ec0190a3c6c8ee2411407c0f29bc7c31d898271
Ahmed Abdulrahman <a.kasapbashi@live.com>
2019-12-29 22:53:04 +0100

    :wrench: [ArticlesHero] update UI

--- @narative/gatsby-theme-novela/src/sections/articles/Articles.Hero.tsx
+++ @narative/gatsby-theme-novela/src/sections/articles/Articles.Hero.tsx
@@ -6,15 +6,17 @@ import Section from '@components/Section';
 import mediaqueries from '@styles/media';
 import { IAuthor } from '@types';
 
-const authorQuery = graphql`
+import Icons from '@icons';
+
+const siteQuery = graphql`
   {
-    site: allSite {
+    allSite {
       edges {
         node {
           siteMetadata {
             hero {
-              writingHeading
               maxWidth
+              writingHeading
             }
           }
         }
@@ -24,115 +26,93 @@ const authorQuery = graphql`
 `;
 
 const ArticlesHero: React.FC<IAuthor> = ({ authors }) => {
-  const results = useStaticQuery(authorQuery);
-  const hero = results.site.edges[0].node.siteMetadata.hero;
-  // const featuredAuthor = authors.find(author => author.featured);
-
-  // if (!featuredAuthor) {
-  //   throw new Error(`
-  //     No featured Author found.
-  //     Please ensure you have at least featured Author.
-  // `);
-  // }
+  const results = useStaticQuery(siteQuery);
+  const hero = results.allSite.edges[0].node.siteMetadata.hero;
 
   return (
-    <Section relative id="Articles__Hero">
-      <HeadingContainer style={{ maxWidth: `${hero.maxWidth}px` }}>
-        <HeroHeading
-          dangerouslySetInnerHTML={{ __html: hero.writingHeading }}
-        />
-      </HeadingContainer>
+    <Section narrow>
+      <Container>
+        <HeroTextContainer>
+          <HeroHeading>{hero.writingHeading}</HeroHeading>
+          {/* <InfoText>{hero.workshop.subHeading}</InfoText> */}
+        </HeroTextContainer>
+        <HeroImage>
+          <Icons.Writing />
+        </HeroImage>
+      </Container>
     </Section>
   );
 };
 
 export default ArticlesHero;
 
-const SubheadingContainer = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  margin-bottom: 50px;
-
-  ${mediaqueries.desktop`
-    margin-bottom: 80px;
-  `};
-
-  ${mediaqueries.tablet`
-    margin-bottom: 50px;
-  `};
-
-  ${mediaqueries.phablet`
-    display: none;
-  `};
-`;
-
-const HeadingContainer = styled.div`
-  font-family: ${p => p.theme.fonts.title};
+const Container = styled.div`
+  position: relative;
+  display: grid;
+  grid-template-columns: 1fr 450px;
+  column-gap: em;
+  overflow: hidden;
   margin: 100px 0;
 
   ${mediaqueries.desktop`
-    width: 80%;
+    grid-template-columns: 1fr 416px;
   `}
-
   ${mediaqueries.tablet`
-    width: 100%;
+    grid-template-columns: 1fr;
+    background: none;
+    margin-bottom: 0;
   `}
+`;
+
+const HeroTextContainer = styled.div`
+  position: relative;
+  align-self: end;
+  align-self: center;
+  ${mediaqueries.tablet`
+    margin: 40px 0 0;
+  `}
+`;
+
+const HeroImage = styled.div`
+  position: relative;
+  align-self: end;
+  ${mediaqueries.tablet`
+    margin-left: 32px;
+    margin-right: 32px;
+    display: none
+  `}
+  svg {
+    fill: ${p => p.theme.colors.accent};
+  }
 `;
 
 const HeroHeading = styled.h1`
   font-style: normal;
   font-weight: 600;
-  font-size: 52px;
-  line-height: 1.15;
+  font-size: 94px;
+  line-height: 98%;
+  letter-spacing: -1px;
+  font-family: ${p => p.theme.fonts.title};
   color: ${p => p.theme.colors.primary};
-
-  a {
-    color: ${p => p.theme.colors.accent};
-  }
-
   ${mediaqueries.desktop`
-    font-size: 38px
+    font-size: 56px
   `}
-
+  ${mediaqueries.tablet`
+    font-size: 48px;
+  `}
   ${mediaqueries.phablet`
-    font-size: 38px;
+    font-size: 44px;
+    line-height: 1.25;
   `}
 `;
 
-const GridButton = styled.button<{ active: boolean }>`
-  position: relative;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 36px;
-  width: 36px;
-  border-radius: 50%;
-  background: transparent;
-  transition: background 0.25s;
-
-  &:hover {
-    background: ${p => p.theme.colors.hover};
-  }
-
-  &[data-a11y='true']:focus::after {
-    content: '';
-    position: absolute;
-    left: -10%;
-    top: -10%;
-    width: 120%;
-    height: 120%;
-    border: 2px solid ${p => p.theme.colors.accent};
-    background: rgba(255, 255, 255, 0.01);
-    border-radius: 50%;
-  }
-
-  svg {
-    opacity: ${p => (p.active ? 1 : 0.25)};
-    transition: opacity 0.2s;
-
-    path {
-      fill: ${p => p.theme.colors.primary};
-    }
-  }
+const InfoText = styled.p`
+  font-size: 18px;
+  margin-top: 24px;
+  line-height: 1.8;
+  font-family: ${p => p.theme.fonts.body};
+  color: ${p => p.theme.colors.secondary};
+  ${mediaqueries.tablet`
+    font-size: 16px;
+  `}
 `;
