5ee75e940e950d1c644fec5151f3ca637f6f5fd2
Ahmed Abdulrahman <a.kasapbashi@live.com>
2020-02-02 10:13:31 +0100

    :wrench: format code

--- @narative/gatsby-theme-novela/src/components/Layout/Layout.tsx
+++ @narative/gatsby-theme-novela/src/components/Layout/Layout.tsx
@@ -31,13 +31,13 @@ const Layout: React.FC<{}> = ({ children }) => {
       </Container>
     </ArticlesContextProvider>
   );
-}
+};
 
 export default Layout;
 
 const Container = styled.div`
   position: relative;
-  background: ${p => p.theme.colors.background};
+  background: ${p => console.log('p', p) || p.theme.colors.background};
   transition: ${p => p.theme.colorModeTransition};
   min-height: 100vh;
 `;
