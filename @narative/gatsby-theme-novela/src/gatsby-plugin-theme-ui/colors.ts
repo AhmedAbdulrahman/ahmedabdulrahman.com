9f58ff7d29f32fdc4651b2751841c1e226668afa
Ahmed Abdulrahman <a.kasapbashi@live.com>
2020-02-28 00:32:04 +0100

    :art: [Colors] update palette

--- @narative/gatsby-theme-novela/src/gatsby-plugin-theme-ui/colors.ts
+++ @narative/gatsby-theme-novela/src/gatsby-plugin-theme-ui/colors.ts
@@ -2,23 +2,30 @@ import prism from './prism';
 
 export default {
   prism,
-
-  primary: '#23477d',
-  secondary: '#38578a',
-  grey: '#6381b3',
-  background: '#ffffff',
-  accent: '#c5d4f1',
+  primary: '#282E44',
+  secondary: '#424862',
+  grey: '#5D637E',
+  background: '#d1dfeb',
+  backgroundModal: 'rgba(209, 215, 235, 0.98)',
+  cleanBackground: '#FFFFFF',
+  accent: '#212c5c',
   hover: 'rgba(0, 0, 0, 0.07)',
-  gradient: 'linear-gradient(180deg, rgba(217, 219, 224, 0) 0%, #e6edf1 100%)',
-  articleText: '#37465f',
-  track: 'rgba(197, 212, 241, 0.7)',
-  progress: '#23477d',
-  card: '#ebf1ff',
+  gradient: 'linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, #FFFFFF 100%)',
+  gradientImage:
+    'linear-gradient(180deg, rgba(209, 232, 235, 0) 0%, rgba(209, 215, 235, 0.95) 71.88%, #d1d7eb 100%)',
+  articleText: '#282E44',
+  track: 'rgba(8, 8, 11, 0.3)',
+  progress: '#000',
+  card: '#fff',
   error: '#EE565B',
   success: '#46B17B',
   errorBackground: 'rgba(238, 86, 91, 0.1)',
   horizontalRule: 'rgba(8, 8, 11, 0.15)',
   inputBackground: 'rgba(0, 0, 0, 0.05)',
+  textTitle: '#111111',
+  neumorphismShadown:
+    '10px 10px 22px rgba(156, 171, 197, 0.28), -10px -10px 24px rgba(240, 243, 250, 0.65)',
+
   modes: {
     dark: {
       grey: '#73737D',
@@ -26,6 +33,8 @@ export default {
       secondary: '#fff',
       accent: '#ffe8a3',
       background: '#1a1c23',
+      backgroundModal: 'rgba(17, 18, 22, 0.95)',
+      cleanBackground: '#111216',
       hover: 'rgba(255, 255, 255, 0.07)',
       gradient:
         'linear-gradient(180deg, #1a1c23 0%, rgba(66, 81, 98, 0.36) 100%)',
@@ -38,6 +47,8 @@ export default {
       errorBackground: 'rgba(238, 86, 91, 0.1)',
       horizontalRule: 'rgba(255, 255, 255, 0.15)',
       inputBackground: 'rgba(255, 255, 255, 0.07)',
+      textTitle: '#fff',
+      neumorphismShadown: 'none',
     },
   },
 };
