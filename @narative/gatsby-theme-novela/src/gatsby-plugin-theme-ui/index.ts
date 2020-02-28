66004e7db733516452f0f4c092d672a2a0229e1a
Ahmed Abdulrahman <a.kasapbashi@live.com>
2020-02-28 01:51:27 +0100

    :art: [Fonts][Weights] adds new font and font weights

--- @narative/gatsby-theme-novela/src/gatsby-plugin-theme-ui/index.ts
+++ @narative/gatsby-theme-novela/src/gatsby-plugin-theme-ui/index.ts
@@ -14,9 +14,14 @@ const breakpoints = [
 ];
 
 const fonts = {
-  title: "'Poppins', 'Helvetica Neue', 'Arial', sans-serif",
-  body: "'Libre Baskerville', Georgia, Serif",
+  title: "'Title', Georgia, Serif",
+  body: "'Text','Libre Baskerville', Georgia, Serif",
   monospace: `"Operator Mono", Consolas, Menlo, Monaco, source-code-pro, Courier New, monospace`,
+};
+
+const fontsWeight = {
+  regular: '300',
+  bold: '600',
 };
 
 const colorModeTransition =
@@ -28,6 +33,7 @@ export default merge({
   colorModeTransition,
   colors,
   fonts,
+  fontsWeight,
   breakpoints,
   tags,
 });
