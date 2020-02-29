7003e23b718df5f7ae04dc80c356f88cdc601f04
Ahmed Abdulrahman <a.kasapbashi@live.com>
2020-02-29 02:45:17 +0100

    :zap: [Pages] adds Contact and Work pages

---
+++ @narative/gatsby-theme-novela/src/pages/contact.tsx
@@ -0,0 +1,331 @@
+import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
+import styled from '@emotion/styled';
+
+import mediaqueries from '@styles/media';
+import { Link } from 'gatsby';
+
+import Section from '@components/Section';
+import SEO from '@components/SEO';
+
+import Layout from '@components/Layout';
+
+import Headings from '@components/Headings';
+
+import { Template } from '@types';
+
+const siteQuery = graphql`
+  {
+    allSite {
+      edges {
+        node {
+          siteMetadata {
+            name
+            siteUrl
+            title
+          }
+        }
+      }
+    }
+  }
+`;
+
+const Contact: Template = ({ location }) => {
+  const results = useStaticQuery(siteQuery);
+  const title = results.allSite.edges[0].node.siteMetadata.title;
+
+  return (
+    <Layout>
+      <SEO pathname={location.href} title={`Contact - ${title}`} />
+      <Section relative>
+        <HeadingContainer>
+          <HeroTextContainer>
+            <HeroHeading>Ask me anything</HeroHeading>
+            <InfoSubHeading>
+              If you have a question please leave a message, I’m always happy to
+              hear from interesting people doing interesting things.
+            </InfoSubHeading>
+          </HeroTextContainer>
+        </HeadingContainer>
+      </Section>
+      <Section>
+        <ContentContainer>
+          <MyText>
+            <form
+              name="contact"
+              netlify-honeypot="bot-field"
+              data-netlify="true"
+              method="POST"
+              action="/success/"
+            >
+              <input type="hidden" name="bot-field" />
+              <input type="hidden" name="form-name" value="contact" />
+              <Label htmlFor="full-name">Full Name</Label>
+              <FormInput
+                name="full-name"
+                type="text"
+                placeholder="Your full name"
+                required
+              />
+              <Label htmlFor="email-address">Email Address</Label>
+              <FormInput
+                name="email-address"
+                type="email"
+                placeholder="Your email"
+                required
+              />
+              <Label htmlFor="message">Message</Label>
+              <FormTextArea
+                name="message"
+                rows="6"
+                placeholder="Your message..."
+                required
+              />
+              <Button type="submit" hasError="" subscribed="">
+                Submit
+              </Button>
+            </form>
+          </MyText>
+        </ContentContainer>
+      </Section>
+      <ArticlesGradient />
+    </Layout>
+  );
+};
+
+export default Contact;
+
+const ArticlesGradient = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 590px;
+  z-index: 0;
+  pointer-events: none;
+  background: ${p => p.theme.colors.gradient};
+  transition: ${p => p.theme.colorModeTransition};
+`;
+
+const HeadingContainer = styled.div`
+  position: relative;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  column-gap: 64px;
+  margin: 100px 0 76px;
+  font-family: ${p => p.theme.fonts.title};
+
+  ${mediaqueries.tablet`
+    grid-template-columns:  1fr;
+    margin-bottom: 0;
+  `}
+`;
+
+const HeroTextContainer = styled.div`
+  position: relative;
+  align-self: end;
+  align-self: center;
+`;
+
+const HeroHeading = styled.h2`
+  font-style: normal;
+  font-weight: ${p => p.theme.fontsWeight.bold};
+  font-size: 86px;
+  line-height: 1;
+  max-width: 700px;
+  color: ${p => p.theme.colors.primary};
+
+  a {
+    color: ${p => p.theme.colors.accent};
+  }
+
+  ${mediaqueries.desktop`
+    font-size: 64px
+  `}
+
+  ${mediaqueries.phablet`
+    font-size: 60px;
+  `}
+`;
+
+const ContentContainer = styled.div`
+  position: relative;
+  display: grid;
+  grid-template-columns: 1fr;
+  column-gap: 64px;
+  z-index: 1;
+`;
+
+const InfoHeading = styled.h1`
+  font-weight: ${p => p.theme.fontsWeight.bold};
+  font-family: ${p => p.theme.fonts.title};
+  font-size: 32px;
+  line-height: 1.35;
+  max-width: 100%;
+  margin-bottom: 32px;
+  color: ${p => p.theme.colors.primary};
+
+  ${mediaqueries.phablet`
+    font-size: 32px;
+  `}
+`;
+
+const InfoSubHeading = styled.p`
+  font-size: 22px;
+  font-family: ${p => p.theme.fonts.body};
+  margin-top: 24px;
+  margin-bottom: 32px;
+  color: ${p => p.theme.colors.primary};
+
+  ${mediaqueries.phablet`
+    font-size: 18px;
+  `}
+`;
+
+const InfoText = styled.p`
+  font-size: 20px;
+  margin-top: 24px;
+  line-height: 1.7;
+  color: ${p => p.theme.colors.primary};
+`;
+
+const MyText = styled.div`
+  position: relative;
+`;
+
+const MyImage = styled.div`
+  position: relative;
+  display: block;
+  width: 100%;
+
+  margin-bottom: 56px;
+
+  .gatsby-image-wrapper {
+    box-shadow: 0 22px 44px 0 rgba(0, 0, 0, 0.22);
+  }
+
+  img {
+    width: 100%;
+    height: auto;
+    object-fit: cover;
+    object-position: center;
+  }
+`;
+
+const NavLink = styled(Link)`
+  transition: ${p => p.theme.colorModeTransition};
+  color: ${p => p.theme.colors.primary};
+  border-bottom: 1px solid ${p => p.theme.colors.primary};
+
+  &:visited {
+    color: ${p => p.theme.colors.primary};
+    opacity: 0.85;
+  }
+
+  &:hover,
+  &:focus {
+    color: ${p => p.theme.colors.accent};
+    border-bottom-color: ${p => p.theme.colors.accent};
+  }
+`;
+
+const FormInput = styled.input`
+  background: #f5f5f5;
+  border: solid 2px #f5f5f5;
+  border-radius: 0;
+  height: 48px;
+  outline-offset: 0;
+  padding: 0 16px;
+  position: relative;
+  transition: all 0.2s;
+  max-width: 100%;
+  width: 100%;
+  -webkit-appearance: none;
+
+  &:focus,
+  &:active {
+    border: 2px solid ${p => p.theme.colors.border};
+    outline: 4px solid ${p => p.theme.colors.outline};
+    outline-offset: 0;
+    transition: all 0.2s;
+  }
+`;
+
+const FormTextArea = styled.textarea`
+  background: #f5f5f5;
+  border: solid 2px #f5f5f5;
+  border-radius: 0;
+  height: auto;
+  resize: none;
+  outline-offset: 0;
+  padding: 10px 15px;
+  position: relative;
+  transition: all 0.2s;
+  max-width: 100%;
+  width: 100%;
+  -webkit-appearance: none;
+
+  &:focus,
+  &:active {
+    border: 2px solid ${p => p.theme.colors.border};
+    outline: 4px solid ${p => p.theme.colors.outline};
+    outline-offset: 0;
+    transition: all 0.2s;
+  }
+`;
+
+const Label = styled.label`
+  display: block;
+  color: ${p => p.theme.colors.primary};
+  margin-top: 15px;
+  margin-bottom: 7.5px;
+`;
+
+const Button = styled.button<{ hasError: string; subscribed: boolean }>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 161px;
+  height: 48px;
+  width: 100%;
+  margin-top: 15px;
+  border: 1px solid
+    ${p => (p.hasError ? p.theme.colors.error : p.theme.colors.accent)};
+  color: ${p => (p.hasError ? p.theme.colors.error : p.theme.colors.accent)};
+  background: ${p => (p.subscribed ? p.theme.colors.accent : 'transparent')};
+  font-weight: ${p => p.theme.fontsWeight.bold};
+  border-radius: 35px;
+  letter-spacing: 0.42px;
+  transition: border-color 0.4s var(--ease-in-out-quad),
+    background 0.4s var(--ease-in-out-quad), color 0.4s var(--ease-in-out-quad),
+    transform 0.4s var(--ease-in-out-quad);
+
+  &:hover {
+    background: ${p =>
+      p.hasError ? p.theme.colors.error : p.theme.colors.accent};
+    color: ${p => p.theme.colors.background};
+    transform: translateY(-2px);
+    box-shadow: 0 10px 28px rgba(47, 47, 47, 0.3);
+  }
+
+  &[disabled] {
+    cursor: not-allowed;
+  }
+`;
+const Error = styled.div`
+  position: absolute;
+  left: 35px;
+  bottom: -20px;
+  color: ${p => p.theme.colors.error};
+  font-size: 12px;
+
+  a {
+    color: ${p => p.theme.colors.error};
+    text-decoration: underline;
+  }
+
+  ${mediaqueries.tablet`
+    left: 50px;
+    top: 50px;
+  `}
+`;
---
+++ @narative/gatsby-theme-novela/src/pages/work-with-me.tsx
@@ -0,0 +1,304 @@
+import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
+import styled from '@emotion/styled';
+import { css } from '@emotion/core';
+// import { useColorMode } from 'theme-ui';
+import mediaqueries from '@styles/media';
+import { Link } from 'gatsby';
+
+import Section from '@components/Section';
+import SEO from '@components/SEO';
+// import Image from '@components/Image';
+import Layout from '@components/Layout';
+// import AnchorLink from '@components/Anchor';
+import Headings from '@components/Headings';
+
+import { Template } from '@types';
+
+const siteQuery = graphql`
+  {
+    allSite {
+      edges {
+        node {
+          siteMetadata {
+            name
+            siteUrl
+            title
+          }
+        }
+      }
+    }
+  }
+`;
+
+const WorkWithMe: Template = ({ location }) => {
+  const results = useStaticQuery(siteQuery);
+  const title = results.allSite.edges[0].node.siteMetadata.title;
+  // const [colorMode] = useColorMode();
+  // const isDark = colorMode === `dark`;
+
+  return (
+    <Layout>
+      <SEO pathname={location.href} title={`Work with me - ${title}`} />
+      <Section relative>
+        <HeadingContainer>
+          <HeroTextContainer>
+            <HeroHeading>Work With Me</HeroHeading>
+            <InfoSubHeading>
+              This page will give you a clear idea of ways we can work together
+              and if I’m the right fit for your next project.
+            </InfoSubHeading>
+          </HeroTextContainer>
+        </HeadingContainer>
+      </Section>
+      <Section>
+        <ContentContainer>
+          <MyText>
+            <InfoHeading>Who I am</InfoHeading>
+            <InfoText>
+              Hello! I'm Ahmed, Creative Front-end Developer and Product
+              Designer from Stockholm, Sweden. I work at Oakwood Creative Studio
+              as Front-end Crispiness Creator, while also taking on freelance
+              projects that pique my interest.
+            </InfoText>
+
+            <InfoHeading
+              css={css`
+                margin-top: 32px;
+                margin-bottom: 0;
+              `}
+            >
+              What I do
+            </InfoHeading>
+            <InfoText>
+              I help businesses and independent individuals turn ideas into
+              fully functional digital products. My work takes place in the grey
+              area between user experience design, user interface design and
+              front-end web development. From research and planning to executing
+              high fidelity prototypes in code, I usually involve myself in
+              projects from concept through to launch.
+            </InfoText>
+
+            <InfoText>
+              I can help you conduct research to develop an understanding of
+              your customers needs, develop wireframes and prototypes to test
+              and validate possible solutions, and build and launch a working
+              product. Through usability testing and iteration I can help you
+              fine tune the product to make improvements.
+            </InfoText>
+
+            <InfoHeading
+              css={css`
+                margin-top: 32px;
+                margin-bottom: 0;
+              `}
+            >
+              How I work
+            </InfoHeading>
+
+            <InfoText>
+              I use recognised strategies and Lean development practices to keep
+              the process as dynamic and fluid as possible. We’ll move
+              thoughtfully and efficiently in the most appropriate direction
+              until we find the right solution.
+            </InfoText>
+
+            <InfoHeading
+              css={css`
+                margin-top: 32px;
+                margin-bottom: 0;
+              `}
+            >
+              Who hires me
+            </InfoHeading>
+            <InfoText>
+              Anyone who need a Front-end Developer &amp; Product Designer
+              experienced in building businesses from the ground up and
+              launching them online.
+            </InfoText>
+
+            <InfoText>
+              My clients tend to be those keen to execute ideas, but don’t
+              necessarily have the technical skills to do so. Or those who’d
+              rather save on big agency fees by investing their budget in good
+              design, at an affordable rate, from an independent designer and
+              developer.
+            </InfoText>
+
+            <InfoHeading
+              css={css`
+                margin-top: 32px;
+                margin-bottom: 0;
+              `}
+            >
+              What I charge
+            </InfoHeading>
+            <InfoText>
+              My rate varies from roughly £1.5k — £2.5k per week, depending on
+              the requirements and my involvement in the project. I charge a
+              flat fee for all projects, so no hourly rate, just a single
+              project price.
+            </InfoText>
+
+            <InfoHeading
+              css={css`
+                margin-top: 32px;
+                margin-bottom: 0;
+              `}
+            >
+              Get in touch
+            </InfoHeading>
+            <InfoText>
+              If you have a project in mind, and you think I might be a good
+              fit, then{' '}
+              <NavLink to={`/contact`} title={`Contact`}>
+                let’s talk about it
+              </NavLink>{' '}
+              and we can take it from there.
+            </InfoText>
+          </MyText>
+        </ContentContainer>
+      </Section>
+      <ArticlesGradient />
+    </Layout>
+  );
+};
+
+export default WorkWithMe;
+
+const ArticlesGradient = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 590px;
+  z-index: 0;
+  pointer-events: none;
+  background: ${p => p.theme.colors.gradient};
+  transition: ${p => p.theme.colorModeTransition};
+`;
+
+const HeadingContainer = styled.div`
+  position: relative;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  column-gap: 64px;
+  margin: 100px 0 76px;
+  font-family: ${p => p.theme.fonts.title};
+
+  ${mediaqueries.desktop`
+    width: 80%;
+  `}
+
+  ${mediaqueries.tablet`
+    width: 100%;
+  `}
+`;
+
+const HeroTextContainer = styled.div`
+  position: relative;
+  align-self: end;
+  align-self: center;
+`;
+
+const HeroHeading = styled.h2`
+  font-size: 85px;
+  line-height: 98%;
+  letter-spacing: -1px;
+  font-family: ${p => p.theme.fonts.title};
+  font-weight: ${p => p.theme.fontsWeight.bold};
+  color: ${p => p.theme.colors.primary};
+
+  ${mediaqueries.desktop`
+    font-size: 56px
+  `}
+  ${mediaqueries.tablet`
+    font-size: 48px;
+  `}
+  ${mediaqueries.phablet`
+    font-size: 56px;
+    line-height: 1.15;
+  `}
+`;
+
+const ContentContainer = styled.div`
+  position: relative;
+  display: grid;
+  grid-template-columns: 1fr;
+  column-gap: 64px;
+  z-index: 1;
+`;
+
+const InfoHeading = styled.h1`
+  font-weight: ${p => p.theme.fontsWeight.bold};
+  font-family: ${p => p.theme.fonts.title};
+  font-size: 32px;
+  line-height: 1.35;
+  max-width: 100%;
+  margin-bottom: 32px;
+  color: ${p => p.theme.colors.primary};
+
+  ${mediaqueries.phablet`
+    font-size: 32px;
+  `}
+`;
+
+const InfoSubHeading = styled.p`
+  font-size: 22px;
+  font-family: ${p => p.theme.fonts.title};
+  margin-top: 24px;
+  margin-bottom: 32px;
+  color: ${p => p.theme.colors.primary};
+
+  ${mediaqueries.phablet`
+    font-size: 18px;
+  `}
+`;
+
+const InfoText = styled.p`
+  font-size: 20px;
+  margin-top: 24px;
+  line-height: 1.5;
+  font-family: ${p => p.theme.fonts.body};
+  color: ${p => p.theme.colors.secondary};
+`;
+
+const MyText = styled.div`
+  position: relative;
+`;
+
+const MyImage = styled.div`
+  position: relative;
+  display: block;
+  width: 100%;
+
+  margin-bottom: 56px;
+
+  .gatsby-image-wrapper {
+    box-shadow: 0 22px 44px 0 rgba(0, 0, 0, 0.22);
+  }
+
+  img {
+    width: 100%;
+    height: auto;
+    object-fit: cover;
+    object-position: center;
+  }
+`;
+
+const NavLink = styled(Link)`
+  transition: ${p => p.theme.colorModeTransition};
+  color: ${p => p.theme.colors.primary};
+  border-bottom: 1px solid ${p => p.theme.colors.primary};
+
+  &:visited {
+    color: ${p => p.theme.colors.primary};
+    opacity: 0.85;
+  }
+
+  &:hover,
+  &:focus {
+    color: ${p => p.theme.colors.accent};
+    border-bottom-color: ${p => p.theme.colors.accent};
+  }
+`;
