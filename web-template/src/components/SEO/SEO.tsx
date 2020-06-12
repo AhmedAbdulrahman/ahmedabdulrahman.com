354291e22f8fb694cd265afa2b3339dfad2a2860
Ahmed Abdulrahman <a.kasapbashi@live.com>
2020-06-12 16:50:46 +0200

    :wrench: [SEO] Big improvement on JSON-LD for SEO

--- web-template/src/components/SEO/SEO.tsx
+++ web-template/src/components/SEO/SEO.tsx
@@ -22,14 +22,19 @@ import Helmet from 'react-helmet';
 import { graphql, useStaticQuery } from 'gatsby';
 
 interface HelmetProps {
-  title: string;
+  articlepathName?: string;
+  authorName?: string;
+  authorsBio?: string;
+  authorsSlug?: string;
+  canonicalUrl?: string;
+  dateforSEO?: string;
   description?: string;
-  pathname: string;
   image?: string;
-  url?: string;
-  canonical?: string;
+  isBlogPost: false;
+  pathname: string;
   published?: string;
   timeToRead?: string;
+  title: string;
 }
 
 const seoQuery = graphql`
@@ -45,6 +50,7 @@ const seoQuery = graphql`
             }
             siteUrl
             title
+            name
           }
         }
       }
@@ -69,24 +75,263 @@ const themeUIDarkModeWorkaroundScript = [
 ];
 
 const SEO: React.FC<HelmetProps> = ({
-  title,
-  description,
+  articlepathName,
+  authorName,
+  authorsBio,
+  authorsSlug,
+  canonicalUrl,
   children,
-  url,
+  dateforSEO,
+  description,
   image,
-  published,
+  isBlogPost,
   pathname,
+  published,
   timeToRead,
+  title,
 }) => {
   const results = useStaticQuery(seoQuery);
   const site = results.allSite.edges[0].node.siteMetadata;
   const twitter = site.social.find(option => option.name === 'twitter') || {};
+  const github = site.social.find(option => option.name === 'github') || {};
+  const behance = site.social.find(option => option.name === 'behance') || {};
+
+  const pageUrl = site.siteUrl + pathname
 
   const fullURL = (path: string) =>
-    path ? `${site.siteUrl}${path}` : site.siteUrl;
+    path ? `${path}` : site.siteUrl;
 
-  // If no image is provided lets looks for a default static image
-  image = image ? image : '/preview.jpg';
+  // If no image is provided lets looks for a default novela static image
+  image = image ? image : `${site.siteUrl}/preview.jpg`;
+
+  // Checks if the source of the image is hosted on Contentful
+  if (`${image}`.includes('ctfassets')) {
+    image = `${image}`;
+  } else {
+    image = fullURL(image);
+  }
+
+  let siteSchema = `{
+    "@context": "https://schema.org",
+    "@graph": [
+      {
+        "@type": "Person",
+        "@id": "${site.siteUrl}/#person",
+        "name": "${site.title}",
+        "url": "${site.siteUrl}",
+        "sameAs": [
+          "${twitter.url}",
+          "${github.url}",
+          "${behance.url}",
+        ],
+        "logo": {
+          "@type": "ImageObject",
+          "@id": "${site.siteUrl}/#logo",
+          "inLanguage": "en-US",
+          "url": "${site.siteUrl}/icons/icon-512x512.png",
+          "width": 512,
+          "height": 512,
+          "caption": "${site.title}"
+        },
+        "image": {
+          "@id": "${site.siteUrl}/#logo"
+        }
+      },
+      {
+        "@type": "WebSite",
+        "@id": "${site.siteUrl}/#website",
+        "url": "${site.siteUrl}",
+        "name": "${site.name}",
+        "description": "${site.description}",
+        "publisher": {
+          "@id": "${site.siteUrl}/#person"
+        },
+        "inLanguage": "en-US"
+      },
+      {
+        "@type": [
+          "WebPage"
+        ],
+        "@id": "${pageUrl}/#webpage",
+        "url": "${pageUrl}",
+        "name": "${title || site.name}",
+        "isPartOf": {
+          "@id": "${site.siteUrl}/#website"
+        },
+        "about": {
+          "@id": "${site.siteUrl}/#person"
+        },
+        "description": "${description || site.description}",
+        "inLanguage": "en-US"
+      },
+      {
+        "@type": "BreadcrumbList",
+        "description": "Breadcrumbs list",
+        "itemListElement": [
+          {
+            "@type": "ListItem",
+            "item": "${site.siteUrl}",
+            "name": "Homepage",
+            "position": "1"
+          }
+        ],
+        "name": "Breadcrumbs"
+      }
+    ]
+  }
+`.replace(/"[^"]+"|(\s)/gm, function (matched, group1) {
+    if (!group1) {
+      return matched;
+    } else {
+      return '';
+    }
+  });
+
+  let blogSchema = `{
+    "@context": "https://schema.org",
+    "@graph": [
+      {
+        "@type": "Person",
+        "@id": "${site.siteUrl}/#person",
+        "name": "${site.title}",
+        "url": "${site.siteUrl}",
+        "sameAs": [
+          "${twitter.url}",
+          "${github.url}",
+          "${behance.url}",
+        ],
+        "logo": {
+          "@type": "ImageObject",
+          "@id": "${site.siteUrl}/#logo",
+          "inLanguage": "en-US",
+          "url": "${site.siteUrl}/icons/icon-512x512.png",
+          "width": 512,
+          "height": 512,
+          "caption": "${site.title}"
+        },
+        "image": {
+          "@id": "${site.siteUrl}/#logo"
+        }
+      },
+      {
+        "@type": "WebSite",
+        "@id": "${site.siteUrl}/#website",
+        "url": "${site.siteUrl}",
+        "name": "${site.name}",
+        "description": "${site.description.replace(/"/g, '\\"')}",
+        "publisher": {
+          "@id": "${site.siteUrl}/#person"
+        },
+        "inLanguage": "en-US"
+      },
+      {
+        "@type": "ImageObject",
+        "@id": "${articlepathName}/#primaryimage",
+        "inLanguage": "en-US",
+        "url": "${image}",
+        "width": 1200,
+        "height": 628
+      },
+      {
+        "@type": [
+          "WebPage"
+        ],
+        "@id": "${articlepathName}/#webpage",
+        "url": "${articlepathName}",
+        "name": "${title}",
+        "isPartOf": {
+          "@id": "${site.siteUrl}/#website"
+        },
+        "primaryImageOfPage": {
+          "@id": "${articlepathName}/#primaryimage"
+        },
+        "datePublished": "${dateforSEO}",
+        "dateModified": "${dateforSEO}",
+        "description": "${description}",
+        "breadcrumb": {
+          "@id": "${articlepathName}/#breadcrumb"
+        },
+        "inLanguage": "en-US"
+      },
+      {
+        "@type": "BreadcrumbList",
+        "@id": "${articlepathName}/#breadcrumb",
+        "itemListElement": [
+          {
+            "@type": "ListItem",
+            "position": 1,
+            "item": {
+              "@type": "WebPage",
+              "@id": "${site.siteUrl}",
+              "url": "${site.siteUrl}",
+              "name": "Home"
+            }
+          },
+          {
+            "@type": "ListItem",
+            "position": 2,
+            "item": {
+              "@type": "WebPage",
+              "@id": "${articlepathName}",
+              "url": "${articlepathName}",
+              "name": "${title}"
+            }
+          }
+        ]
+      },
+      {
+        "@type": "Article",
+        "@id": "${articlepathName}/#article",
+        "isPartOf": {
+          "@id": "${articlepathName}/#webpage"
+        },
+        "author": {
+          "@id": "${site.siteUrl}/#/schema${authorsSlug}"
+        },
+        "headline": "${title}",
+        "datePublished": "${dateforSEO}",
+        "dateModified": "${dateforSEO}",
+        "mainEntityOfPage": {
+          "@id": "${articlepathName}/#webpage"
+        },
+        "publisher": {
+          "@id": "${site.siteUrl}/#person"
+        },
+        "image": {
+          "@id": "${articlepathName}/#primaryimage"
+        },
+        "inLanguage": "en-US"
+      },
+      {
+        "@type": [
+          "Person"
+        ],
+        "@id": "${site.siteUrl}/#/schema${authorsSlug}",
+        "name": "${authorName}",
+        "image": {
+          "@type": "ImageObject",
+        "@id": "${site.siteUrl}/#personlogo",
+          "inLanguage": "en-US",
+          "caption": "${authorName}"
+        },
+        "description": "${authorsBio}",
+        "sameAs": [
+          "${twitter.url}",
+          "${github.url}",
+          "${behance.url}"
+        ]
+      }
+    ]
+  }
+`.replace(/"[^"]+"|(\s)/gm, function (matched, group1) {
+    if (!group1) {
+      return matched;
+    } else {
+      return '';
+    }
+  });
+
+  const schema = isBlogPost ? blogSchema : siteSchema
 
   const metaTags = [
     { charset: 'utf-8' },
@@ -100,35 +345,29 @@ const SEO: React.FC<HelmetProps> = ({
     },
     {
       name: 'theme-color',
-      content: '#faf9d5',
-    },
-    {
-      name: 'apple-mobile-web-app-status-bar-style',
-      content: 'black-translucent',
+      content: '#fff',
     },
     { itemprop: 'name', content: title || site.title },
     { itemprop: 'description', content: description || site.description },
-    { itemprop: 'image', content: fullURL(image) },
+    { itemprop: 'image', content: image },
     { name: 'description', content: description || site.description },
 
     { name: 'twitter:card', content: 'summary_large_image' },
-    { name: 'twitter:site', content: '@_ahmed_ab' || site.name },
+    { name: 'twitter:site', content: site.name },
     { name: 'twitter:title', content: title || site.title },
     { name: 'twitter:description', content: description || site.description },
-    { name: 'twitter:url', content: twitter.url },
+    { name: 'twitter:creator', content: twitter.url },
     {
       name: 'twitter:image',
-      content: fullURL(image),
+      content: image,
     },
 
+    { property: 'og:type', content: 'website' },
     { property: 'og:title', content: title || site.title },
-    { property: 'og:url', content: url || site.siteUrl },
-    { property: 'og:type"', content: 'website' },
-    { property: 'og:image', content: fullURL(image) },
-    { property: 'og:image:alt', content: "Ahmed Abdulrahman's Desk" },
+    { property: 'og:url', content: articlepathName || pageUrl },
+    { property: 'og:image', content: image },
     { property: 'og:description', content: description || site.description },
     { property: 'og:site_name', content: site.name },
-    { property: 'og:locale', content: 'en' },
   ];
 
   if (published) {
@@ -136,8 +375,8 @@ const SEO: React.FC<HelmetProps> = ({
   }
 
   if (timeToRead) {
-    metaTags.push({ name: 'twitter:label1', content: 'Reading time' });
-    metaTags.push({ name: 'twitter:data1', content: `${timeToRead} min read` });
+    metaTags.push({ name: 'twitter:label1', value: 'Reading time' });
+    metaTags.push({ name: 'twitter:data1', value: `${timeToRead} min read` });
   }
 
   return (
@@ -147,7 +386,8 @@ const SEO: React.FC<HelmetProps> = ({
       script={themeUIDarkModeWorkaroundScript}
       meta={metaTags}
     >
-      <link rel="icon" type="image/png" href="/icon-72x72.png"></link>
+      <script type="application/ld+json">{schema}</script>
+      {canonicalUrl && <link rel="canonical" href={canonicalUrl} />}
       {children}
     </Helmet>
   );
