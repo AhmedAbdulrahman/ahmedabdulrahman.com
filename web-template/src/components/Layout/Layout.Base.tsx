2d7796e70681d78abe68bc437841dc7d21421b96
Ahmed Abdulrahman <a.kasapbashi@live.com>
2020-12-07 03:33:01 +0100

    :wrench: [Layout] use Cursor & SmoothScroll

--- web-template/src/components/Layout/Layout.Base.tsx
+++ web-template/src/components/Layout/Layout.Base.tsx
@@ -5,32 +5,47 @@ import { useColorMode } from 'theme-ui';
 
 import NavigationFooter from '@components/Navigation/Navigation.Footer';
 import NavigationHeader from '@components/Navigation/Navigation.Header';
+import Cursor from '@components/Cursor';
 import ArticlesContextProvider from '../../sections/articles/Articles.List.Context';
 
 import { globalStyles } from '@styles';
-
+import { Scroll } from '@hooks';
 /**
  * <Layout /> needs to wrap every page as it provides styles, navigation,
  * and the main structure of each page. Within Layout we have the <Container />
  * which hides a lot of the mess we need to create our Desktop and Mobile experiences.
  */
-const LayoutBase: React.FC<{}> = ({ children }) => {
+interface LayoutProps {
+  children: ReactNode
+  location: Location
+}
+
+const LayoutBase = ({ children } : LayoutProps) : JSX.Element => {
   const [colorMode] = useColorMode();
-  const isDark = colorMode === `dark`;
+  const cursorColor = colorMode === 'dark' ? '255,255,255' : '254, 231, 162'
 
   useEffect(() => {
     parent.postMessage({ theme: colorMode }, '*');
   }, [colorMode]);
 
   return (
-    <ArticlesContextProvider>
-      <Container>
-        <Global styles={globalStyles} isDark={isDark} />
-        <NavigationHeader />
-        {children}
-        <NavigationFooter />
-      </Container>
-    </ArticlesContextProvider>
+      <ArticlesContextProvider>
+        <Scroll callbacks={location} />
+        <Cursor
+          innerSize={10}
+          outerSize={40}
+          color={cursorColor}
+          outerAlpha={0.2}
+          innerScale={0.5}
+          outerScale={2}
+          />
+        <Container id="container" data-scroll-container>
+            <Global styles={globalStyles}/>
+            <NavigationHeader />
+              {children}
+            <NavigationFooter />
+        </Container>
+      </ArticlesContextProvider>
   );
 };
 
@@ -42,4 +57,8 @@ const Container = styled.div`
   transition: ${p => p.theme.colorModeTransition};
   min-height: 100vh;
   z-index: 1;
+  margin: 0 auto;
+  padding:  1.0875rem 1.45rem;
+  /* margin: 0 auto; */
+  /* padding: 120px 1.45rem 1.0875rem 1.45rem; */
 `;
--- web-template/src/components/Layout/Layout.tsx
+++ web-template/src/components/Layout/Layout.tsx
@@ -1,21 +1,29 @@
-import React, { useEffect } from 'react';
+import React, { ReactNode, useEffect } from 'react';
 import { Global } from '@emotion/core';
 import styled from '@emotion/styled';
 import { useColorMode } from 'theme-ui';
 
 import NavigationFooter from '@components/Navigation/Navigation.Footer';
 import NavigationHeader from '@components/Navigation/Navigation.Header';
+import Cursor from '@components/Cursor';
 import ArticlesContextProvider from '../../sections/articles/Articles.List.Context';
 
 import { globalStyles } from '@styles';
-
+import { Scroll } from '@hooks';
 /**
  * <Layout /> needs to wrap every page as it provides styles, navigation,
  * and the main structure of each page. Within Layout we have the <Container />
  * which hides a lot of the mess we need to create our Desktop and Mobile experiences.
  */
-const Layout: React.FC<{}> = ({ children }) => {
+
+interface LayoutProps {
+   children: ReactNode
+   location: Location
+ }
+
+const Layout = ({ children, location }: LayoutProps) : JSX.Element => {
   const [colorMode] = useColorMode();
+  const cursorColor = colorMode === 'dark' ? '255,255,255' : '254, 231, 162'
 
   useEffect(() => {
     parent.postMessage({ theme: colorMode }, '*');
@@ -23,13 +31,23 @@ const Layout: React.FC<{}> = ({ children }) => {
 
   return (
     <ArticlesContextProvider>
-      <Container>
-        <Global styles={globalStyles} />
-        <NavigationHeader />
-        {children}
-        <NavigationFooter />
-      </Container>
-    </ArticlesContextProvider>
+        {/* Here we pass the callbacks to the component. Anything that impacts the innerHeight, for example: Font Loaded */}
+        <Scroll callbacks={location} />
+        <Cursor
+          innerSize={10}
+          outerSize={40}
+          color={cursorColor}
+          outerAlpha={1}
+          innerScale={0.5}
+          outerScale={2}
+        />
+        <Container id="container" data-scroll-container>
+          <Global styles={globalStyles} />
+          <NavigationHeader />
+            {children}
+          <NavigationFooter />
+        </Container>
+      </ArticlesContextProvider>
   );
 };
 
@@ -41,4 +59,6 @@ const Container = styled.div`
   transition: ${p => p.theme.colorModeTransition};
   min-height: 100vh;
   z-index: 1;
+  margin: 0 auto;
+  padding:  1.0875rem 1.45rem;
 `;
