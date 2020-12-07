41c059952d7f8cdafd2f662ca1778b44f05ccc5a
Ahmed Abdulrahman <a.kasapbashi@live.com>
2020-12-07 03:31:51 +0100

    :package: [npm] adds LocomotiveScroll package

---
+++ web-template/src/theme.ts
@@ -0,0 +1,13 @@
+// eslint-disable-next-line import/prefer-default-export
+export const scrollOptions = {
+	// Locomotive Scroll
+	// https://github.com/locomotivemtl/locomotive-scroll#instance-options
+	container: `#container`,
+	options: {
+		smooth: true,
+		smoothMobile: false,
+		getDirection: true,
+		touchMultiplier: 2.5,
+		lerp: 0.15,
+	},
+}
